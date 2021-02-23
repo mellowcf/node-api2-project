a3aca1be2d92a8cf3f1b7229ad854cdb0cafefb5
Sterling Ford <mellowcf@gmail.com>
2021-02-22 22:46:45 -0500

    mvp

--- knexfile.js
+++ knexfile.js
@@ -1,26 +1,18 @@
-const sharedConfig = {
-  client: 'sqlite3',
-  useNullAsDefault: true,
-  migrations: {
-    directory: './data/migrations',
-  },
-  seeds: {
-    directory: './data/seeds',
-  },
-  pool: {
-    afterCreate: (conn, done) => {
-      conn.run('PRAGMA foreign_keys = ON', done)
-    },
-  },
-}
-
 module.exports = {
   development: {
-    ...sharedConfig,
+    client: 'sqlite3',
     connection: { filename: './data/lambda.db3' },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './data/migrations',
+    },
+    seeds: {
+      directory: './data/seeds',
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      },
+    },
   },
-  testing: {
-    ...sharedConfig,
-    connection: { filename: './data/testing.db3' },
-  },
-}
+};
